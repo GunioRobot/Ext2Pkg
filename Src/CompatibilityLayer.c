195ebc5614e176348fd08ed56a4ea2ae72550039
Alin Rus <netblock@gmail.com>
2011-07-02 06:30:02 +0300

    Add bswap(N) & memcpy compatibility layer

--- Src/CompatibilityLayer.c
+++ Src/CompatibilityLayer.c
@@ -14,3 +14,54 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 **/
 
 #include "CompatibilityLayer.h"
+
+/**
+Switches the endianness of a 16-bit integer.
+
+This function swaps the bytes in a 16-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value     A 16-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint16_t bswap16(uint16_t Value)
+{
+  return SwapBytes16(Value);
+}
+
+/**
+Switches the endianness of a 32-bit integer.
+
+This function swaps the bytes in a 32-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value A 32-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint32_t bswap32(uint32_t Value)
+{
+  return SwapBytes32(Value);
+}
+
+/**
+Switches the endianness of a 64-bit integer.
+
+This function swaps the bytes in a 64-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value A 64-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint64_t bswap64(uint64_t Value)
+{
+  return SwapBytes64(Value);
+}
--- Src/CompatibilityLayer.h
+++ Src/CompatibilityLayer.h
@@ -17,6 +17,12 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 #ifndef _COMPATIBILITYLAYER_H_
 #define _COMPATIBILITYLAYER_H_
 
+#define memcpy CopyMem
+
+#undef bswap16
+#undef bswap32
+#undef bswap64
+
 typedef UINT8 u_char;
 
 typedef INT8 int8_t;
@@ -31,4 +37,45 @@ typedef UINT64 uint64_t;
 typedef INT64 off_t;
 typedef INT64 daddr_t;
 
+/**
+Switches the endianness of a 16-bit integer.
+
+This function swaps the bytes in a 16-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value     A 16-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint16_t bswap16(uint16_t Value);
+
+/**
+Switches the endianness of a 32-bit integer.
+
+This function swaps the bytes in a 32-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value A 32-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint32_t bswap32(uint32_t Value);
+
+/**
+Switches the endianness of a 64-bit integer.
+
+This function swaps the bytes in a 64-bit unsigned value to switch the value
+from little endian to big endian or vice versa. The byte swapped value is
+returned.
+
+@param  Value A 64-bit unsigned value.
+
+@return The byte swapped Value.
+
+**/
+uint64_t bswap64(uint64_t Value);
 #endif

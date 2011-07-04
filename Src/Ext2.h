a3f679055f11021f117af3109e106d2e81ec3a7e
Alin Rus <netblock@gmail.com>
2011-07-04 04:31:17 +0300

    Add EFI_FILE_PROTOCOL function declarations

--- Src/Ext2.h
+++ Src/Ext2.h
@@ -31,6 +31,9 @@
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/DevicePathLib.h>
+#include <Guid/FileSystemInfo.h>
+#include <Guid/FileInfo>
+#include <Guid/FileSystemVolumeLabelInfo.h>
 
 #include "ext2fs.h"
 
@@ -53,7 +56,34 @@ typedef struct {
 } EXT2_DEV;
 
 #define EXT2_DEV_FROM_THIS(a) CR (a, EXT2_DEV, BlockIo, EXT2_PRIVATE_DATA_SIGNATURE)
-#define EXT2_DEV_FROM_FILESYSTEM(a) CR (a, EXT2_DEV, Filesystem, EXT2_PRIVATE_DATA_SIGNATURE)
+#define EXT2_SIMPLE_FILE_SYSTEM_PRIVATE_DATA_FROM_THIS(a) \
+	CR (a, \
+	EXT2_DEV, \
+	Filesystem, \
+	EXT2_PRIVATE_DATA_SIGNATURE\
+	)
+
+#define EXT2_FILE_PRIVATE_DATA_SIGNATURE SIGNATURE_32 ('E','x','t','f')
+
+typedef struct {
+  UINTN           Signature;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *Filesystem;
+  EFI_FILE_PROTOCOL EfiFile;
+  INTN            fd;
+  DIR            *Dir;
+  BOOLEAN         IsRootDirectory;
+  BOOLEAN         IsDirectoryPath;
+  BOOLEAN         IsOpenedByRead;
+  char           *FileName;
+  struct dirent  *Dirent;
+} EXT2_EFI_FILE_PRIVATE;
+
+#define EXT2_EFI_FILE_PRIVATE_DATA_FROM_THIS(a) \
+	CR (a, \
+	EXT2_EFI_FILE_PRIVATE, \
+	EfiFile, \
+	EXT2_FILE_PRIVATE_DATA_SIGNATURE SIGNATURE_32 \
+	)
 
 //
 // Global Variables
@@ -130,6 +160,37 @@ EFI_STATUS EFIAPI Ext2Stop (
   IN EFI_HANDLE * ChildHandleBuffer
 );
 
+/*++
+
+Routine Description:
+
+  Open the root directory on a volume.
+
+Arguments:
+
+  This  - A pointer to the volume to open.
+
+  Root  - A pointer to storage for the returned opened file handle of the root directory.
+
+Returns:
+
+  EFI_SUCCESS           - The volume was opened.
+
+  EFI_UNSUPPORTED       - The volume does not support the requested file system type.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupted.
+
+  EFI_ACCESS_DENIED     - The service denied access to the file.
+
+  EFI_OUT_OF_RESOURCES  - The file volume could not be opened due to lack of resources.
+
+  EFI_MEDIA_CHANGED     - The device has new media or the media is no longer supported.
+
+--*/
 
 EFI_STATUS EFIAPI Ext2OpenVolume (
   IN EFI_SIMPLE_FILE_SYSTEM_PROTOCOL * This,
@@ -272,9 +333,335 @@ EFI_STATUS EFIAPI Ext2ComponentNameGetControllerName (
 
 **/
 EFI_STATUS      Ext2CheckSB (
-  IN EFI_DISK_IO_PROTOCOL *DiskIo,
+  IN EFI_DISK_IO_PROTOCOL * DiskIo,
   IN UINT32 MediaId,
   OUT struct ext2fs *e2fs
 );
 
+
+/*++
+
+Routine Description:
+
+  Open a file relative to the source file location.
+
+Arguments:
+
+  This        - A pointer to the source file location.
+
+  NewHandle   - Pointer to storage for the new file handle.
+
+  FileName    - Pointer to the file name to be opened.
+
+  OpenMode    - File open mode information.
+
+  Attributes  - File creation attributes.
+
+Returns:
+
+  EFI_SUCCESS           - The file was opened.
+
+  EFI_NOT_FOUND         - The file could not be found in the volume.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_MEDIA_CHANGED     - The device has new media or the media is no longer supported.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupted.
+
+  EFI_WRITE_PROTECTED   - The volume or file is write protected.
+
+  EFI_ACCESS_DENIED     - The service denied access to the file.
+
+  EFI_OUT_OF_RESOURCES  - Not enough resources were available to open the file.
+
+  EFI_VOLUME_FULL       - There is not enough space left to create the new file.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemOpen (
+  IN EFI_FILE_PROTOCOL * This,
+  OUT EFI_FILE_PROTOCOL ** NewHandle,
+  IN CHAR16 * FileName,
+  IN UINT64 OpenMode,
+  IN UINT64 Attributes
+);
+
+/*++
+
+Routine Description:
+
+  Close the specified file handle.
+
+Arguments:
+
+  This  - Pointer to a returned opened file handle.
+
+Returns:
+
+  EFI_SUCCESS - The file handle has been closed.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemClose (
+  IN EFI_FILE_PROTOCOL * This
+);
+
+/*++
+
+Routine Description:
+
+  Close and delete a file.
+
+Arguments:
+
+  This  - Pointer to a returned opened file handle.
+
+Returns:
+
+  EFI_SUCCESS             - The file handle was closed and deleted.
+
+  EFI_WARN_DELETE_FAILURE - The handle was closed but could not be deleted.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemDelete (
+  IN EFI_FILE_PROTOCOL * This
+);
+
+/*++
+
+Routine Description:
+
+  Read data from a file.
+
+Arguments:
+
+  This        - Pointer to a returned open file handle.
+
+  BufferSize  - On input, the size of the Buffer.  On output, the number of bytes stored in the Buffer.
+
+  Buffer      - Pointer to the first byte of the read Buffer.
+
+Returns:
+
+  EFI_SUCCESS           - The data was read.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupted.
+
+  EFI_BUFFER_TOO_SMALL  - The supplied buffer size was too small to store the current directory entry.
+                          *BufferSize has been updated with the size needed to complete the request.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemRead (
+  IN EFI_FILE_PROTOCOL * This,
+  IN OUT UINTN * BufferSize,
+  OUT VOID * Buffer
+);
+
+/*++
+
+Routine Description:
+
+  Write data to a file.
+
+Arguments:
+
+  This        - Pointer to an opened file handle.
+
+  BufferSize  - On input, the number of bytes in the Buffer to write to the file.  On output, the number of bytes
+                of data written to the file.
+
+  Buffer      - Pointer to the first by of data in the buffer to write to the file.
+
+Returns:
+
+  EFI_SUCCESS           - The data was written to the file.
+
+  EFI_UNSUPPORTED       - Writes to an open directory are not supported.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupt.
+
+  EFI_WRITE_PROTECTED   - The file, directory, volume, or device is write protected.
+
+  EFI_ACCESS_DENIED     - The file was opened read-only.
+
+  EFI_VOLUME_FULL       - The volume is full.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemWrite (
+  IN EFI_FILE_PROTOCOL * This,
+  IN OUT UINTN * BufferSize,
+  IN VOID * Buffer
+);
+
+/*++
+
+Routine Description:
+
+  Set a file's current position.
+
+Arguments:
+
+  This      - Pointer to an opened file handle.
+
+  Position  - The byte position from the start of the file to set.
+
+Returns:
+
+  EFI_SUCCESS     - The file position has been changed.
+
+  EFI_UNSUPPORTED - The seek request for non-zero is not supported for directories.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemSetPosition (
+  IN EFI_FILE_PROTOCOL * This,
+  IN UINT64 Position
+);
+
+/*++
+
+Routine Description:
+
+  Get a file's current position.
+
+Arguments:
+
+  This      - Pointer to an opened file handle.
+
+  Position  - Pointer to storage for the current position.
+
+Returns:
+
+  EFI_SUCCESS     - The file position has been reported.
+
+  EFI_UNSUPPORTED - Not valid for directories.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemGetPosition (
+  IN EFI_FILE_PROTOCOL * This,
+  OUT UINT64 * Position
+);
+
+/*++
+
+Routine Description:
+
+  Return information about a file or volume.
+
+Arguments:
+
+  This            - Pointer to an opened file handle.
+
+  InformationType - GUID describing the type of information to be returned.
+
+  BufferSize      - On input, the size of the information buffer.  On output, the number of bytes written to the
+                    information buffer.
+
+  Buffer          - Pointer to the first byte of the information buffer.
+
+Returns:
+
+  EFI_SUCCESS           - The requested information has been written into the buffer.
+
+  EFI_UNSUPPORTED       - The InformationType is not known.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupt.
+
+  EFI_BUFFER_TOO_SMALL  - The buffer size was too small to contain the requested information.  The buffer size has
+                          been updated with the size needed to complete the requested operation.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemGetInfo (
+  IN EFI_FILE_PROTOCOL * This,
+  IN EFI_GUID * InformationType,
+  IN OUT UINTN * BufferSize,
+  OUT VOID * Buffer
+);
+
+/*++
+
+Routine Description:
+
+  Set information about a file or volume.
+
+Arguments:
+
+  This            - Pointer to an opened file handle.
+
+  InformationType - GUID identifying the type of information to set.
+
+  BufferSize      - Number of bytes of data in the information buffer.
+
+  Buffer          - Pointer to the first byte of data in the information buffer.
+
+Returns:
+
+  EFI_SUCCESS           - The file or volume information has been updated.
+
+  EFI_UNSUPPORTED       - The information identifier is not recognised.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures are corrupt.
+
+  EFI_WRITE_PROTECTED   - The file, directory, volume, or device is write protected.
+
+  EFI_ACCESS_DENIED     - The file was opened read-only.
+
+  EFI_VOLUME_FULL       - The volume is full.
+
+  EFI_BAD_BUFFER_SIZE   - The buffer size is smaller than the type indicated by InformationType.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemSetInfo (
+  IN EFI_FILE_PROTOCOL * This,
+  IN EFI_GUID * InformationType,
+  IN UINTN BufferSize,
+  IN VOID * Buffer
+);
+
+/*++
+
+Routine Description:
+
+  Flush all modified data to the media.
+
+Arguments:
+
+  This  - Pointer to an opened file handle.
+
+Returns:
+
+  EFI_SUCCESS           - The data has been flushed.
+
+  EFI_NO_MEDIA          - The device has no media.
+
+  EFI_DEVICE_ERROR      - The device reported an error.
+
+  EFI_VOLUME_CORRUPTED  - The file system structures have been corrupted.
+
+  EFI_WRITE_PROTECTED   - The file, directory, volume, or device is write protected.
+
+  EFI_ACCESS_DENIED     - The file was opened read-only.
+
+  EFI_VOLUME_FULL       - The volume is full.
+
+--*/
+EFI_STATUS EFIAPI Ext2SimpleFileSystemFlush (
+  IN EFI_FILE_PROTOCOL * This
+);
 #endif
